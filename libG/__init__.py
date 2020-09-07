b53d7973d2f0f4132e968544d26904d561398a91
Hammam Abdelwahab <Hammam.Abdelwahab@iais.fraunhofer.de>
2020-09-07 16:06:32 +0200

    removing content from master

--- libG/__init__.py
+++
@@ -1,8 +0,0 @@
-from .hclust_sort import hclust_sort
-from .plot_matrix import plot_matrix
-from .outlier_1d import outlier_1d
-from .Report import Report
-from .plot_decision_tree import plot_decision_tree
-from .travelling_salesman import solve_tsp, approximate_tsp
-from .modelling import model_and_feature_selection
-from .plotting import plot_feature_importance, plot_true_v_pred, plot_partial_dependence
--- tests/test_menu.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from snippetlib.menu import MenuHandler as mh 
-
-class TestMenuHandler(unittest.TestCase):
-      def test_menu(self):
-          s = mh.menu()
-          self.assertIsNotNone(s)
-
