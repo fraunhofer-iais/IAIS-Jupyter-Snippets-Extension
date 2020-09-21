3fc24b3e105edc41e24b931bf46c31ed4c82fed8
Hammam Abdelwahab <Hammam.Abdelwahab@iais.fraunhofer.de>
2020-09-21 14:46:29 +0200

    return notebook and ipywidget dependencies to setup.py

--- setup.py
+++ setup.py
@@ -141,9 +141,11 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
 
+    
     install_requires= ['ipywidgets<=7.5.1',
     'notebook<=6.0.3',
-    ]
+    ],  # Optional
+   
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
